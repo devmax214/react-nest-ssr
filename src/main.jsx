ac3d34bde5c6a851ff52fe955799c379e189674e
Devmax <maxsamuel214@protonmail.com>
2023-11-01 03:47:36 -0700

    Edit route

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import ErrorPage from "./error-page";
+import EditContact, { action as editAction } from "./routes/edit";
 import './index.css'
 
 const router = createBrowserRouter([
@@ -15,11 +16,18 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
