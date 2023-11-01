62f75a43020cc091b77260bdba373ff918404de5
Devmax <maxsamuel214@protonmail.com>
2023-11-01 01:50:23 -0700

    Add router

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,24 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import ErrorPage from "./error-page";
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 
