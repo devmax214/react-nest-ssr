f0d36d0c776e70c19b85b5b3caf4aeb1a2bdf2b9
Devmax <maxsamuel214@protonmail.com>
2023-11-01 02:15:27 -0700

    Add nest config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeModule } from './recipe/recipe.module';
 import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [RecipeModule, ClientModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }), 
+    RecipeModule, 
+    ClientModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/client/client.service.ts
+++ backend/src/client/client.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as fs from 'fs';
 import * as path from 'path';
 
 @Injectable()
 export class ClientService {
+  constructor(private configService: ConfigService) {}
+
   public async getApp() {
     const basePath = this.configService.get('CLIENT_BUILD_PATH');
     const filePath = path.resolve(path.join(basePath, 'index.html'));
