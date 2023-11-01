47d6a9f8787bb3539a17129a85c6e6927171711e
Devmax <maxsamuel214@protonmail.com>
2023-11-01 02:08:05 -0700

    Add client

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeModule } from './recipe/recipe.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [RecipeModule],
+  imports: [RecipeModule, ClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
